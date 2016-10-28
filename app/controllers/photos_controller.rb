39e91873519d7291605eacd260bf913ec44ce701
Murugan <murugu2012@gmail.com>
2016-10-28 19:14:33 +0530

    Generate photos resource with starter:resource

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,50 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new
+    @photo.user_id = params[:user_id]
+    @photo.caption = params[:caption]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    @photo.user_id = params[:user_id]
+    @photo.caption = params[:caption]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+
+    @photo.destroy
+
+    redirect_to "/photos", :notice => "Photo deleted."
+  end
+end
